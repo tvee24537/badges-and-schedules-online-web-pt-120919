571fa057c0a0a5fe75fc61a79a6074bb37d62f8a
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-19 04:44:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+name_list = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(name_list)
+  name_list.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(name_list)
+  name_list.each
+end  
